fff74e8e67b5b930dcbf8e2409ec8d40806d59be
CHKiens <44415938+CHKiens@users.noreply.github.com>
2024-10-07 02:16:11 +0200

    Employee og Location SQL

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
+using PersoApp.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<PersoAppDBContext>();
 
 var app = builder.Build();
 
