233138db102d803171b361d296d290c8178b07bf
Birk Jefta Svensson <bis001@edu.zealand.dk>
2024-10-08 14:50:46 +0200

    Create new employee er lavet. Vi kan evt. lave en tabel med forskellige jobs, hvor salary afhænger af job type

---
+++ Pages/CreateEmployee.cshtml.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using PersoApp.Interfaces;
+using PersoApp.Models;
+
+namespace PersoApp.Pages
+{
+    public class CreateEmployeeModel : PageModel
+    {
+        private IEmployee repository;
+        private ILocation locationRepo;
+        [BindProperty]
+        public Employee employee {  get; set; }
+        public List<Location> locations { get; set; }
+
+
+        public CreateEmployeeModel(IEmployee repo, ILocation locationRepo)
+        {
+            repository = repo;
+            this.locationRepo = locationRepo;
+        }
+
+        public void OnGet()
+        {
+            locations = locationRepo.GetAllLocations();
+        }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            
+            repository.AddEmployee(employee);
+            return RedirectToPage("Employees");
+        }
+    }
+}
--- Services/EmployeeService.cs
+++ Services/EmployeeService.cs
@@ -1,7 +1,20 @@
-﻿using PersoApp.Interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using PersoApp.Interfaces;
+using PersoApp.Models;
 
 namespace PersoApp.Services {
     public class EmployeeService :IEmployee {
 
+        private PersoAppDBContext _Context;
+        public EmployeeService(PersoAppDBContext context)
+        {
+            _Context = context;
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            _Context.Add(employee);
+            _Context.SaveChanges();
+        }
     }
 }
